9ec8fb060386f6aa37051fe3d65bd4f2112dd59f
Diga Karki <digakarki107@gmail.com>
2022-11-07 13:34:23 +0545

    First version added to git

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <footer className="py-1">
+                <p className="text-center mt-1">
+                    BeautyPrism Website By Diga Karki
+                </p>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
